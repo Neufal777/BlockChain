931525425c9a5a1380360ab9e9d3348c1e3cdecf
gilmiriam <miriam.gil@lifullconnect.com>
2020-03-15 19:44:33 +0100

    change package name

--- domain/blockchain.go
+++ domain/blockchain.go
@@ -1,14 +1,14 @@
-package main
+package domain
 
 type Blockchain struct {
-	blocks []*Block
+	Blocks []*Block
 }
 
 func (bc *Blockchain) AddBlock(data string) {
 
-	prevBlock := bc.blocks[len(bc.blocks)-1]
+	prevBlock := bc.Blocks[len(bc.Blocks)-1]
 	newBlock := newBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
+	bc.Blocks = append(bc.Blocks, newBlock)
 
 }
 
--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 //Develop branch
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/NCoin/domain"
+)
 
 func main() {
 
-	bc := NewBlockchain()
+	bc := domain.NewBlockchain()
 
 	bc.AddBlock("0.6 BTC")
 	bc.AddBlock("0.3 BTC")
 	bc.AddBlock("0.1 BTC")
 
-	for _, block := range bc.blocks {
+	for _, block := range bc.Blocks {
 
 		fmt.Printf("Previous block hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
